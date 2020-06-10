fd2f3a5cfca7d4ed723d14267b751589d4eed423
Marvin G P <marvin.guerrero@hotmail.com>
2020-06-09 19:15:29 -0500

    agregando rutas

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.end('Hola Mundo.!');
-})
+});
+
+app.get('/login', (req, res) => {
+    res.end('Login.!');
+});
+
+app.get('*', (req, res) => {
+    res.end('Pagina no encontrada');
+});
+
 app.listen(3000, () => {
     console.log('Server funcionando');
 });
